ef4a96405f9187ec2ab70290afcd0a8c4db10e50
Konstantin Hantsov <hantsov@cantemo.com>
2020-10-08 20:56:59 +0200

    Add number of grades to stats

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from prettytable import PrettyTable
 
 
 def schools(args):
-    file_name = f'data/Odata{args.year}_utf8.csv'
+    file_name = f'data/Odata{args.year}.csv'
     s = set()
 
     with open(file_name, newline='') as f:
@@ -35,7 +35,7 @@ def top(args):
 
 
 def print_top(year, sortby='TOTAL', sortorder='DESC', areaname=None, N=10, verbose=False):
-    file_name = f'data/Odata{year}_utf8.csv'
+    file_name = f'data/Odata{year}.csv'
 
 
     columns = [
@@ -141,7 +141,7 @@ def language(args):
     )
 
 def print_stats_by(year, grouping='SEXTYPENAME', grouping_title='SEX', sortby='TOTAL', sortorder='DESC', areaname=None, N=10, verbose=False):
-    file_name = f'data/Odata{year}_utf8.csv'
+    file_name = f'data/Odata{year}.csv'
 
 
     columns = [
@@ -164,7 +164,7 @@ def print_stats_by(year, grouping='SEXTYPENAME', grouping_title='SEX', sortby='T
 
     results = defaultdict(lambda: defaultdict(list))
     
-
+    c = 0
     with open(file_name, newline='') as f:
         reader = csv.DictReader(f, delimiter=';')
         
@@ -177,21 +177,23 @@ def print_stats_by(year, grouping='SEXTYPENAME', grouping_title='SEX', sortby='T
             for column in columns:
                 if row[column] != 'null':
                     results[grouping_value][column].append(float(row[column].replace(',','.')))
+            
+            c += 1
 
     stats_totals = defaultdict(dict)
 
     for grouping_value, stats in results.items():
         stats_totals[grouping_value] = {
-            k.replace('Ball100', '').upper(): round(statistics.mean(v), 2)
+            k.replace('Ball100', '').replace('BALL100', '').upper(): (round(statistics.mean(v), 2), len(v))
             for k, v in sorted(stats.items(), key=lambda x: x[0])
         }
         all_marks = list(itertools.chain(*list(stats.values())))
         
         if all_marks:
-            stats_totals[grouping_value]['TOTAL'] = round(statistics.mean(all_marks), 2)
+            stats_totals[grouping_value]['TOTAL'] = (round(statistics.mean(all_marks), 2), len(all_marks))
 
     stats_totals = sorted(
-        stats_totals.items(), key=lambda x: x[1].get(sortby, 0),
+        stats_totals.items(), key=lambda x: x[1].get(sortby, [0, 0])[0],
         reverse=sortorder == 'DESC'
     )
 
@@ -199,18 +201,19 @@ def print_stats_by(year, grouping='SEXTYPENAME', grouping_title='SEX', sortby='T
 
     table = PrettyTable()
     if verbose:
-        table.field_names = ['#', grouping_title, "year"] + [x.replace('Ball100', '').upper() for x in columns] + ['MEAN SCORE']
+        table.field_names = ['#', grouping_title] + [x.replace('Ball100', '').upper() for x in columns] + ['MEAN SCORE']
     else:
-        table.field_names = ["#", grouping_title, "year", sortby, "MEAN SCORE"]
+        table.field_names = ["#", grouping_title, sortby, "MEAN SCORE"]
 
     table.align[grouping_title] = 'l'
 
     for number, (grouping_value, gender_stats_total) in enumerate(stats_totals[:N], start=1):
         if verbose:
-            table.add_row([number, grouping_value, year] + [gender_stats_total.get(x, 0.0) for x in _columns] + [gender_stats_total["TOTAL"]])
+            table.add_row([number, grouping_value] + [f'{gender_stats_total.get(x, [0.0, 0])[0]} ({gender_stats_total.get(x, [0.0, 0])[1]})' for x in _columns] + [f'{gender_stats_total["TOTAL"][0]} ({gender_stats_total["TOTAL"][1]})'])
         else:
-            table.add_row([number, grouping_value, year, gender_stats_total[sortby], gender_stats_total["TOTAL"]])
+            table.add_row([number, grouping_value, f'{gender_stats_total[sortby][0]} ({gender_stats_total[sortby][1]})', f'{gender_stats_total["TOTAL"][0]} ({gender_stats_total["TOTAL"][1]})'])
 
+    print(f'Stats for {year}. Analysed {c} records')
     print(table)
 
 
